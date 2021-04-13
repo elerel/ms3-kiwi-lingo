d04ca6c86be85f76027eadac1f79a6fc811b28d0
elerel <elcranley@gmail.com>
2021-04-13 12:15:57 +0000

    Added registration functionality

--- app.py
+++ app.py
@@ -27,8 +27,26 @@ def get_words():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == 'POST':
+        #check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username")})
+        if existing_user:
+            flash("Yeah, nah mate. Username already in use.")
+            return redirect(url_for("register"))
+
+        register = {
+            "first_name": request.form.get("first_name"),
+            "username": request.form.get("username"),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username")
+        flash("Ripper! You've created your profile page!")
     return render_template("register.html")
-    
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
     $(".sidenav").sidenav({
         edge: "right"
     });
+
+    // Code snippet (to confirm pw's match) from Stackoverflow https://stackoverflow.com/questions/21727317/how-to-check-confirm-password-field-in-form-without-reloading-page/21727518    
+    $('#password, #confirm_password').on('keyup', function () {
+        if ($('#password').val() == $('#confirm_password').val()) {
+            $('#message').html('Matching').css('color', 'green');
+        } else
+            $('#message').html('Not Matching').css('color', 'red');
+    });
 });
