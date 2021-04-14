ea9e700fdfa6ec9494e203cbb4fc3a04c73d7d93
elerel <elcranley@gmail.com>
2021-04-14 09:47:37 +0000

    Created task collapse setup from Materialize

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $(".sidenav").sidenav({
         edge: "right"
     });
+    $('.collapsible').collapsible();
 
     // Code snippet (to confirm pw's match) from Stackoverflow https://stackoverflow.com/questions/21727317/how-to-check-confirm-password-field-in-form-without-reloading-page/21727518    
     $('#password, #confirm_password').on('keyup', function () {
