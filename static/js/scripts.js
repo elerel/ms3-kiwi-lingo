a33d293bdd08869672d96e43b853f24fdeaf476c
elerel <elcranley@gmail.com>
2021-04-19 10:44:51 +0000

    feat: added  materialize jquery modal functionality

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
     $("select").formSelect();
+    $('.modal').modal();
 
     // Code snippet (to confirm pw's match) from Stackoverflow https://stackoverflow.com/questions/21727317/how-to-check-confirm-password-field-in-form-without-reloading-page/21727518    
     $('#password, #confirm_password').on('keyup', function () {
