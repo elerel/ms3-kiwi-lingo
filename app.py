b2d9c23af9eb8a7a9ccf51c0061e545e243ccb5c
elerel <elcranley@gmail.com>
2021-04-20 11:28:19 +0000

    feat: added manage categories template

--- app.py
+++ app.py
@@ -163,6 +163,11 @@ def delete_word(word_id):
     return redirect(url_for("get_words"))
 
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
